6921c80593962474c56231f58d6f7f0bdc05d8e2
Prasanna Bisen <prasannabisen@192.168.1.4>
2024-06-08 19:09:55 +0530

    test.py added

---
+++ test.py
@@ -0,0 +1,39 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS, Chroma
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+import numpy as np
+
+import streamlit as st
+import os 
+from dotenv import load_dotenv
+
+st.title("Hdfc gpt")
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+loader = PyPDFLoader("Vehicle Finance.pdf")
+# data = loader.load_and_split()
+dataLoad = loader.load()
+
+text_spliter = RecursiveCharacterTextSplitter()
+text = text_spliter.split_documents(dataLoad)
+# db = FAISS.from_documents(text, OpenAIEmbeddings())
+db = Chroma(persist_directory="Rupyy", embedding_function=OpenAIEmbeddings())
+llm = Ollama(model="llama3")
+
+prompt = ChatPromptTemplate.from_template('''Answer the following question only on context provided and give detailed answer and also references of the answer
+                                          <context>
+                                          {context}
+                                          </context>
+                                          Question:{input}''')
+document_chain=create_stuff_documents_chain(llm, prompt)
+retriver = db.as_retriever()
+retrival_chain = create_retrieval_chain(retriver, document_chain)
+input_data = st.text_input("Enter input text")
+if input_data:
+    result = retrival_chain.invoke({"input": input_data})
+    st.write("Result:", result["answer"])
